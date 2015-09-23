6636eb6941267325ddfd23496b58b179c830cac4
pyrospade-mac <pyro_quiro@hotmail.com>
2015-09-23 20:19:39 +0200

    Initial Commit

---
+++ MapApp OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MapApp OSX
+//
+//  Created by Albert Quiroga Bertolin on 23/9/15.
+//  Copyright © 2015 Albert Quiroga Bertolin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MapApp OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MapApp OSX
+//
+//  Created by Albert Quiroga Bertolin on 23/9/15.
+//  Copyright © 2015 Albert Quiroga Bertolin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
